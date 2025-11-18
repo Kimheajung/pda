76cef9aeebeecb456dd2dcb575594085b03d5c6c
김혜정 <amykimheajung@gmail.com>
2025-11-18 15:24:07 +0900

    fix: correct AG Grid enterprise license

--- src/components/aggrid/CustomAgGrid.jsx
+++ src/components/aggrid/CustomAgGrid.jsx
@@ -25,7 +25,7 @@ import { LicenseManager, AllEnterpriseModule } from 'ag-grid-enterprise';
 if (typeof window !== 'undefined') {
   try {
     LicenseManager.setLicenseKey(
-      'Using_this_{AG_Grid}_Enterprise_key_{AG-105090}_...'
+      'Using_this_{AG_Grid}_Enterprise_key_{AG-105090}_in_excess_of_the_licence_granted_is_not_permitted___Please_report_misuse_to_legal@ag-grid.com___For_help_with_changing_this_key_please_contact_info@ag-grid.com___{CIT_CO.,_Ltd}_is_granted_a_{Multiple_Applications}_Developer_License_for_{4}_Front-End_JavaScript_developers___All_Front-End_JavaScript_developers_need_to_be_licensed_in_addition_to_the_ones_working_with_{AG_Grid}_Enterprise___This_key_has_not_been_granted_a_Deployment_License_Add-on___This_key_works_with_{AG_Grid}_Enterprise_versions_released_before_{14_November_2026}____[v3]_[01]_MTc5NDYxNDQwMDAwMA==1195a6d5489c5d8fda25776bfbfb07fb'
     );
     ModuleRegistry.registerModules([AllEnterpriseModule]);
   } catch (e) {
