a007c4d7dbf0b996497ef724a71228780c373e93
김혜정 <amykimheajung@gmail.com>
2025-12-26 13:36:08 +0900

    initial commit (pda)

--- src/components/aggrid/DefaultColumnWidths.jsx
+++
@@ -1,4 +0,0 @@
-// 공통 컬럼 너비 설정
-export const defaultColumnWidths = {
-  '#': 70,
-};
--- src/util/Moment.jsx
+++
@@ -1,5 +0,0 @@
-import moment from 'moment';
-
-moment.locale('ko');
-
-export const formatter = (dateStr, format) => moment(dateStr, 'YYYYMMDDHHmmss').format(format);
