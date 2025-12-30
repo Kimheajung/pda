a86e5fd427c3995f2756feec7270214070cd99cd
김혜정 <amykimheajung@gmail.com>
2025-12-30 14:13:58 +0900

    모바일

--- src/AppFooter.jsx
+++ src/AppFooter.jsx
@@ -5,30 +5,38 @@ export const AppFooter = (props) => {
 
     return (
         <div className="layout-footer" >
+            <Link to="/" >
            <button
                 type="button"
                 className="p-link layout-menu-button flex flex-col items-center gap-1.5 text-white" >
                 <i className="pi pi-qrcode" />
                 <span className="text-sm">입고</span>
             </button>
+            </Link>
+             <Link to="/" >
              <button
                 type="button"
                 className="p-link layout-menu-button flex flex-col items-center gap-1.5 text-white" >
                 <i className="pi pi-truck" />
                 <span className="text-sm">출고</span>
             </button>
+            </Link>
+             <Link to="/" >
              <button
                 type="button"
                 className="p-link layout-menu-button flex flex-col items-center gap-1.5 text-white" >
                 <i className="pi pi-tablet" />
                 <span className="text-sm">기타</span>
             </button>
+            </Link>
+             <Link to="/" >
              <button
                 type="button"
-                className="p-link layout-menu-button flex flex-col items-center gap-1.5 text-white" onClick={props.onToggleMenuClick}>
+                className="p-link layout-menu-button flex flex-col items-center gap-1.5 text-white">
                 <i className="pi pi-ellipsis-h" />
                 <span className="text-sm">더보기</span>
             </button>
+             </Link>
         </div>
     );
 }
--- src/page/dashboard/DashboardMain.jsx
+++ src/page/dashboard/DashboardMain.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Button } from 'primereact/button';
 import { Card } from 'primereact/card';
+import { TabView, TabPanel } from 'primereact/tabview';
 // import { ProductService } from '../service/ProductService';
 
 
@@ -28,9 +29,49 @@ const DashboardMain = (props) => {
         `}
       </style>
 
-      <div className="flex justify-center items-center w-full h-full">
-        <img src="/pda/images/main.png" alt="main" className="max-w-none"  />
-      </div>
+       <div className="card">  
+      
+      
+              {/* 공통 case01 : 검색영역 + 그리드 버튼 + 그리드영역 */}
+              <div className="hugreen_grid flex-1 flex flex-wrap md:flex-row">
+      
+                <div className="hugreen_mobile_wrap">
+                  <TabView className="hugreen-main-tabview" activeIndex={0}>
+                      <TabPanel header="입고">
+                          <div className="flex justify-center items-center w-full h-full">
+                            <img src="/pda/images/main.png" alt="main" className="max-w-none"  />
+                          </div>
+                      </TabPanel>
+                      <TabPanel header="출고">
+                          <p className="m-0">
+                              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, 
+                              eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo
+                              enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui 
+                              ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.
+                          </p>
+                      </TabPanel>
+                      <TabPanel header="기타">
+                          <p className="m-0">
+                              At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti 
+                              quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in
+                              culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. 
+                              Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
+                          </p>
+                      </TabPanel>
+                  </TabView>
+                </div>
+                 
+      
+                  
+                  
+      
+      
+                  
+                 
+      
+      
+              </div>      
+          </div>
     </>
   );
 };
