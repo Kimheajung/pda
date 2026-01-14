f57c2573c4fc3c6c1ccdc0ccab4bb3d3cb5e8960
김혜정 <amykimheajung@gmail.com>
2026-01-14 11:08:59 +0900

    ss

--- src/AppFooter.jsx
+++ src/AppFooter.jsx
@@ -9,7 +9,7 @@ export const AppFooter = (props) => {
            <button
                 type="button"
                 className="p-link layout-menu-button flex flex-col items-center gap-1.5 text-white" >
-                <i className="pi pi-qrcode text-[20px]" />
+                <i className="pi pi-qrcode" style={{ fontSize: "20px"}} />
                 <span className="text-sm">입고</span>
             </button>
             </Link>
@@ -17,7 +17,7 @@ export const AppFooter = (props) => {
              <button
                 type="button"
                 className="p-link layout-menu-button flex flex-col items-center gap-1.5 text-white" >
-                <i className="pi pi-truck text-[20px]"  />
+                <i className="pi pi-truck" style={{ fontSize: "20px"}} />
                 <span className="text-sm">출고</span>
             </button>
             </Link>
@@ -25,14 +25,14 @@ export const AppFooter = (props) => {
              <button
                 type="button"
                 className="p-link layout-menu-button flex flex-col items-center gap-1.5 text-white" >
-                <i className="pi pi-tablet text-[20px]" />
+                <i className="pi pi-tablet" style={{ fontSize: "20px"}} />
                 <span className="text-sm">기타</span>
             </button>
             </Link>
              <button
                 type="button"
                 className="p-link layout-menu-button flex flex-col items-center gap-1.5 text-white" onClick={props.onToggleMenuClick}>
-                <i className="pi pi-ellipsis-h text-[20px]" />
+                <i className="pi pi-ellipsis-h" style={{ fontSize: "20px"}} />
                 <span className="text-sm">더보기</span>
             </button>
         </div>
